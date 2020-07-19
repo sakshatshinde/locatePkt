924caf2f40721465361ada756e1b11e0d5383061
Sakshat Shinde <shindesakshat@gmail.com>
2020-07-19 12:41:23 +0530

    Module now creates a list
    This module now creates a list and stores all the collected IPs into it.

--- capture.py
+++ capture.py
@@ -6,11 +6,14 @@ from scapy.all import sniff
 # Number of records to be sniffed
 COUNT = 50
 
+collectIp = []
+
 ## Define our Custom Action function
 def custom_action(packet):
     #get destination keys
-    print(packet[0][1].dst)
+    pktDest = (packet[0][1].dst)
+    collectIp.append(pktDest)
 
 ## Setup sniff, filtering for IP traffic
 sniff(filter="ip", prn=custom_action, count = COUNT)
-
+print(collectIp)
